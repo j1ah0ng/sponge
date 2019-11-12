53e9a2b1c23cbc2eb45e973a7ed0de0295384f6b
Jiahong Long <jiahong888long@gmail.com>
2019-11-12 15:03:32 -0800

    would you look at that documentation is a thing

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ OUT = sponge
 sponge: sponge.cc sponge.h
 	$(CC) -o $(OUT) $(CFLAGS) sponge.cc
 
-install: sponge configure
+install: clean sponge
 	ln -s $(shell pwd)/sponge /home/${USER}/.local/bin/sponge 
 
 clean:
--- sponge.h
+++ sponge.h
@@ -7,6 +7,12 @@ static const char NOT_EXCEPT[] = {'c', 'i', 'L', 'k', 'O', 's',
                                   'U', 'v', 'w', 'x', 'z'};
 static constexpr int REN = static_cast<int>(sizeof(EXCEPTIONS));
 
+/* a disgustingly simple state machine with two states
+ * 1. upper case
+ * 2. lower case
+ * the transition is defined by whether or not the total number of upper case
+ * characters is less than the total number of lower case characters.
+ */
 typedef struct State_Machine {
   int consec_up;
   int consec_down;
